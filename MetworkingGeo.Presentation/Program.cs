52d2a15d10360e406aee16f23eda8450ea1e2f84
Caio Ragazzi <ca.ragazzi@gmail.com>
2021-04-12 11:43:47 -0300

    Adicionando eureka

--- MetworkingGeo.Presentation/Program.cs
+++ MetworkingGeo.Presentation/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Discovery.Eureka;
 
 namespace MetworkingGeo.Presentation
 {
@@ -12,6 +14,7 @@ namespace MetworkingGeo.Presentation
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .AddServiceDiscovery(optionsAction => optionsAction.UseEureka())
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
